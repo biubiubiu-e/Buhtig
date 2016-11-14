a8e2869938b374eaedfc8c8e8895661669910149
Robert Xie <admin@xieaoran.com>
2016-11-15 00:50:21 +0800

    BackEnd Implementations

---
+++ Buhtig.Tests/GitTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using Buhtig.Configs;
+using Buhtig.Models.User;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Buhtig.Tests
+{
+    [TestClass]
+    public class GitTest
+    {
+        [TestMethod]
+        public void CloneTest()
+        {
+            RuntimeConfigs.LoadDefaults();
+            if (!Directory.Exists(RuntimeConfigs.LocalWorkSpaceConfig.WorkingDir))
+            {
+                Directory.CreateDirectory(RuntimeConfigs.LocalWorkSpaceConfig.WorkingDir);
+            }
+            var xieaoran = new Student(1140310201, "Robert Xie", 18846441857L, "Robert Xie", "admin@xieaoran.com");
+            var team = new Team("TestTeam", new[] {xieaoran}, new Uri("https://github.com/xieaoran/library"));
+        }
+    }
+}
---
+++ Buhtig/Configs/LocalWorkSpaceConfig.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Buhtig.Configs
+{
+    public class LocalWorkSpaceConfig
+    {
+        public string WorkingDir { get; set; }
+        public string RepoPathFormat { get; set; }
+
+        public LocalWorkSpaceConfig()
+        {
+            var appData = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            var appName = Resources.Strings.AppName;
+            WorkingDir = Path.Combine(appData, appName, Resources.Strings.RepoFolder);
+            RepoPathFormat = Path.Combine(WorkingDir, "{0}");
+        }
+    }
+}
